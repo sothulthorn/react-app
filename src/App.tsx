2304c71e9d7be28487141229dcaa239eacc158bf
sothulthorn <thornsothul@gmail.com>
2026-01-01 19:36:44 +0900

    Building Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import ListGroup from './components/ListGroup';
+import Button from './components/Button';
+import Alert from './components/Alert';
+import { useState } from 'react';
 
 function App() {
-  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Berlin'];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [alertVisible, setAlertVisible] = useState(false);
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisible(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
