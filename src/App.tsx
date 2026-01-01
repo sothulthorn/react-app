0c1f48c052e449615bfa4766731fd9d35023fcb3
sothulthorn <thornsothul@gmail.com>
2026-01-01 19:16:56 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import ListGroup from './components/ListGroup';
+
+function App() {
+  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Berlin'];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
